38028876481d53d723c54b7e7a39447c85bd57c4
Austin Wise <AustinWise@gmail.com>
2022-02-24 07:20:50 -0800

    Add some code that generates PortablePDB tables.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        const int MY_CONST = 42;
+        Console.WriteLine(MY_CONST);
+    }
+
+    public static IEnumerable<int> StateMachineTest(int a, int b)
+    {
+        int c = a;
+        for (int i = 0; i < 10; i++)
+        {
+            c += b;
+            yield return c;
+        }
+    }
+
+    public static void LocalVariableTest()
+    {
+        var kvp = KeyValuePair.Create(1, 2);
+        Console.WriteLine(kvp.Key);
+    }
+}
